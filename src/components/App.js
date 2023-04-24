fe0400aeb9b0d17d91525233ae1f0f4ea9ba3521
Alexander Repeta <alexander.repeta@gmail.com>
2023-04-24 20:34:06 +0300

    add examples

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,29 @@ import { GlobalStyle } from './GlobalStyle';
 import { Layout } from './Layout/Layout';
 import { RecipeForm } from './RecipeForm/RecipeForm';
 
+// render > didMount > getItem > setState > update > render > didUpdate > setItem
+
 export class App extends Component {
   state = {
-    recipes: initialRecipes,
+    recipes: [],
   };
+
+  componentDidMount() {
+    const savedRecipes = localStorage.getItem('recipes');
+    if (savedRecipes !== null) {
+      // Если сохранили в LS уже что-то, пишем ЭТО в state
+      this.setState({ recipes: JSON.parse(savedRecipes) });
+    } else {
+      // Если в LS ничего еще нет, пишем в state initialRecipes
+      this.setState({ recipes: initialRecipes });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.recipes !== this.state.recipes) {
+      localStorage.setItem('recipes', JSON.stringify(this.state.recipes));
+    }
+  }
 
   addRecipe = newRecipe => {
     this.setState(prevState => ({
@@ -23,6 +42,7 @@ export class App extends Component {
   };
 
   render() {
+    console.log('render');
     return (
       <Layout>
         <RecipeForm onSave={this.addRecipe} />
--- src/index.js
+++ src/index.js
@@ -19,9 +19,9 @@ const theme = {
 };
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+  // </React.StrictMode>
 );
